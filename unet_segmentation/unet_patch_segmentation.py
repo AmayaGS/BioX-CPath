ccf3d853d1f394e6125df1a6bf9039920169e12b
AmayaGS <a.r.syed@qmul.ac.uk>
2024-03-11 15:59:21 +0000

    unet patch segmentation

--- unet_segmentation/unet_patch_segmentation.py
+++ unet_segmentation/unet_patch_segmentation.py
@@ -5,35 +5,84 @@ Created on Tue Feb 20 16:27:20 2024
 @author: AmayaGS
 """
 
+# Misc
 import os
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import argparse
 
+# Hellslide
 import openslide as osi
 
+# PIL
 from PIL import ImageFile
 import PIL.Image as Image
 Image.MAX_IMAGE_PIXELS = None
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 Image.warnings.simplefilter('error', Image.DecompressionBombWarning)
 
+# PyTorch
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
+# Patchification
 from empatches_mod import EMPatches
 emp = EMPatches()
 
+# UNET model
 from Models import UNet_512
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 import gc
 gc.enable()
 
-#%%
 
-class Data_Generator(Dataset):
+
+def arg_parse():
+
+    parser = argparse.ArgumentParser(description="Input arguments for unet segmentation and patching of Whole Slide Images")
+
+    parser.add_argument('--input_directory', type=str, default= r"D:\R4RA_slides/", help='Input data directory')
+    parser.add_argument('--results_directory', type=str, default= "D:\R4RA_patches/", help='Results directory path')
+    parser.add_argument('--path_to_checkpoints', type=str, required=True, help='Path to model checkpoints')
+    parser.add_argument('--NUM_WORKERS', type=int, default=0, help='Number of workers (default: 0)')
+    parser.add_argument('--PIN_MEMORY', type=bool, default=False, help='Pin memory (default: False)')
+    parser.add_argument('--patchsize', type=int, default=224, help='Patch size (default: 224)')
+    parser.add_argument('--overlap', type=int, default=0, help='Overlap (default: 0)')
+    parser.add_argument('--shuffle', type=bool, default=False, help='Shuffle (default: False)')
+    parser.add_argument('--keep_patches', type=bool, default=True, help='Keep patches (default: True)')
+    parser.add_argument('--coverage', type=float, default=0.3, help='Coverage (default: 0.3)')
+    parser.add_argument('--slide_batch', type=int, default=1, help='Slide batch (default: 1)')
+    parser.add_argument('--batch_size', type=int, default=10, help='Batch size (default: 10)')
+    parser.add_argument('--slide_level', type=int, default=2, help='Slide level (default: 2)')
+    parser.add_argument('--mean', nargs='+', type=float, default=[0.8946, 0.8659, 0.8638], help='Mean (default: [0.8946, 0.8659, 0.8638])')
+    parser.add_argument('--std', nargs='+', type=float, default=[0.1050, 0.1188, 0.1180], help='Standard deviation (default: [0.1050, 0.1188, 0.1180])')
+
+
+    args = parser.parse_args()
+
+    return args
+
+def batch_generator(items, batch_size):
+    count = 1
+    chunk = []
+
+    for item in items:
+        if count % batch_size:
+            chunk.append(item)
+        else:
+            chunk.append(item)
+            yield chunk
+            chunk = []
+        count += 1
+
+    if len(chunk):
+        yield chunk
+
+
+class patches_loader(Dataset):
 
     def __init__(self, image_dir, results_dir, transform, slide_level, patchsize, overlap):
 
@@ -42,6 +91,8 @@ class Data_Generator(Dataset):
         self.images = os.listdir(image_dir)
         self.transform = transform
         self.slide_level = slide_level
+        self.patchsize = patchsize
+        self.overlap = overlap
 
     def __len__(self):
         return len(self.images)
@@ -71,7 +122,7 @@ class Data_Generator(Dataset):
                 print(f"Slide {img_name} max magnification level is {properties['openslide.objective-power']}")
                 image = np.array(slide.read_region((0, 0), self.slide_level, slide.level_dimensions[self.slide_level]).convert('RGB'))
 
-            img_patches, img_indices = emp.extract_patches(image, patchsize=patchsize, overlap=overlap)
+            img_patches, img_indices = emp.extract_patches(image, patchsize=self.patchsize, overlap=self.overlap)
 
             if self.transform:
                 img_patches = [self.transform(img) for img in img_patches]
@@ -83,36 +134,20 @@ class Data_Generator(Dataset):
             return self.__getitem__(index)
 
 
-def slide_dataloader(image_dir, results_dir, transform, slide_level, patchsize, overlap, slide_batch, num_workers, pin_memory,shuffle):
+def slide_loader(image_dir, results_dir, transform, slide_level, patchsize, overlap, slide_batch, num_workers, pin_memory,shuffle):
 
-    dataset = Data_Generator(image_dir=image_dir, results_dir=results_dir, transform=transform,slide_level=slide_level,patchsize=patchsize, overlap=overlap)
+    dataset = patches_loader(image_dir=image_dir, results_dir=results_dir, transform=transform, slide_level=slide_level, patchsize=patchsize, overlap=overlap)
     slide_loader = DataLoader(dataset, batch_size=slide_batch, num_workers=num_workers, pin_memory=pin_memory, shuffle=shuffle)
 
     return slide_loader
 
 
-#%%
 
-def batch_generator(items, batch_size):
-    count = 1
-    chunk = []
-
-    for item in items:
-        if count % batch_size:
-            chunk.append(item)
-        else:
-            chunk.append(item)
-            yield chunk
-            chunk = []
-        count += 1
-
-    if len(chunk):
-        yield chunk
-
-def create_mask_and_patches(test_loader, model, batch_size, mean, std, device, path_to_save_mask_and_df, path_to_save_patches, coverage):
+def create_mask_and_patches(test_loader, model, batch_size, mean, std, device, path_to_save_mask_and_df, path_to_save_patches, coverage, keep_patches):
 
     loop = tqdm(test_loader)
     model.eval()
+
     with torch.no_grad():
 
         f = open(path_to_save_mask_and_df + "extracted_patches.csv", "a+")
@@ -175,50 +210,34 @@ def create_mask_and_patches(test_loader, model, batch_size, mean, std, device, p
 
         f.close
 
-#%%
 
-#### Results Paths #####
-path_to_save_mask_and_df = r"D:\R4RA_patches\results/"
-path_to_save_patches = r"D:\R4RA_patches/"
 
-#%%
+def main(args):
 
-#### Loading Paths #####
-test_img_dir = r"D:\R4RA_slides/"
-path_to_checkpoints = r"C:\Users\Amaya\Documents\PhD\IHC-segmentation\IHC_segmentation\IHC_Synovium_Segmentation\UNet weights\UNet_512_1.pth.tar"
+    # Loading Paths
+    path_to_save_mask_and_df = args.results_directory + "/results/"
 
-#%%
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(args.mean, args.std)])
 
-# Variables
-NUM_WORKERS = 0
-PIN_MEMORY = False
-patchsize = 224
-overlap = 0
-shuffle = False
-keep_patches = True
-coverage = 0.3
-slide_batch = 1
-batch_size = 10
-slide_level = 2
-mean = (0.8946, 0.8659, 0.8638)
-std = (0.1050, 0.1188, 0.1180)
+    loader = slide_loader(args.input_directory, args.results_directory, transform, args.slide_level, args.patchsize, args.overlap,
+                               args.slide_batch, args.NUM_WORKERS, args.PIN_MEMORY, args.shuffle)
+
+    Model = UNet_512().to(device=DEVICE, dtype=torch.float)
+    checkpoint = torch.load(args.path_to_checkpoints, map_location=DEVICE)
+    Model.load_state_dict(checkpoint['state_dict'], strict=True)
+
+    create_mask_and_patches(loader, Model, args.batch_size, args.mean, args.std, DEVICE, path_to_save_mask_and_df,
+                            args.results_directory, args.coverage)
+
+
 
 #%%
 
-test_transform = transforms.Compose([
-                 transforms.ToTensor(),
-                 transforms.Normalize(mean, std)])
-
-test_loader = slide_dataloader(test_img_dir, path_to_save_patches, test_transform, slide_level ,patchsize, overlap, slide_batch, NUM_WORKERS, PIN_MEMORY, shuffle)
-print(len(test_loader)) ### this shoud be = Total_images/ batch size'''
-#a = next(iter(test_loader)) # This is to test
-
-#%%
-
-Model = UNet_512().to(device=DEVICE,dtype=torch.float)
-checkpoint = torch.load(path_to_checkpoints, map_location=DEVICE)
-Model.load_state_dict(checkpoint['state_dict'], strict=True)
-create_mask_and_patches(test_loader, Model, batch_size, mean, std, DEVICE, path_to_save_mask_and_df, path_to_save_patches, coverage)
 
 
-#%%
+
+if __name__ == "__main__":
+    args = arg_parse()
+    main(args)
