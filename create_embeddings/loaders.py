22247cc83328bc1448d07007a36fba9bd23b49e7
AmayaGS <a.r.syed@qmul.ac.uk>
2024-06-17 13:03:43 +0100

    Update loaders.py

--- create_embeddings/loaders.py
+++ create_embeddings/loaders.py
@@ -15,15 +15,20 @@ from torch.utils.data import Dataset
 
 class histoDataset(Dataset):
 
-    def __init__(self, df, transform, label):
+    def __init__(self, df, transform, label, multistain):
 
         self.transform = transform
         self.labels = df[label].astype(int).tolist()
         self.filepaths = df['File_location'].tolist()
-        self.patient_ID = df['Patient_ID'].tolist()
-        self.file = df['Filename'].tolist()
-        self.patch_name = df['Patch_name'].tolist()
+        self.patient_IDs = df['Patient_ID'].tolist()
+        self.filenames = df['Filename'].tolist()
+        self.patch_names = df['Patch_name'].tolist()
         self.coordinates = df['Patch_coordinates'].tolist()
+
+        self.multistain = multistain
+
+        if self.multistain:
+            self.stain_types = df['Stain_type'].tolist()
 
     def __len__(self):
         return len(self.labels)
@@ -36,34 +41,37 @@ class histoDataset(Dataset):
             # If the image has an alpha channel, remove it
             if image.mode == 'RGBA':
                 image = image.convert('RGB')
-            patient_id = self.patient_ID[idx]
-            file = self.file[idx]
-            patch_name = self.patch_name[idx]
-            coordinates = self.coordinates[idx]
+            patient_id = self.patient_IDs[idx]
+            filename = self.filenames[idx]
+            patch_name = self.patch_names[idx]
+            coordinate = self.coordinates[idx]
             self.image_tensor = self.transform(image)
             self.image_label = self.labels[idx]
 
-            return self.image_tensor, self.image_label, patient_id, file, patch_name, coordinates
+            if self.multistain:
+                stain_type = self.stain_types[idx]
 
-        except FileNotFoundError:
-            patient_id = self.patient_ID[idx]
-            file = self.file[idx]
-            patch_name = self.patch_name[idx]
-            coordinates = self.coordinates[idx]
-            return []
+                return self.image_tensor, self.image_label, patient_id, filename, patch_name, coordinate, stain_type
+
+            else:
+
+                return self.image_tensor, self.image_label, patient_id, filename, patch_name, coordinate
+
+        except (FileNotFoundError, IndexError):
+            return self.__getitem__(idx)
 
 
 class Loaders:
 
-        def slides_dataloader(self, df, ids, transform, slide_batch, num_workers, shuffle, collate, label='Pathotype_binary', patient_id="Patient_ID"):
+        def slides_dataloader(self, df, ids, transform, slide_batch, num_workers, shuffle, collate, label, patient_id, multistain):
 
             # TRAIN dict
             patient_subsets = {}
 
             for i, file in enumerate(ids):
                 new_key = f'{file}'
-                patient_subset = histoDataset(df[df[patient_id] == file], transform, label=label)
+                patient_subset = histoDataset(df[df[patient_id] == file], transform, label, multistain)
     #            if len(train_subset) != 0:
-                patient_subsets[new_key] = torch.utils.data.DataLoader(patient_subset, batch_size=10, shuffle=shuffle, num_workers=num_workers, drop_last=False, collate_fn=collate)
+                patient_subsets[new_key] = torch.utils.data.DataLoader(patient_subset, batch_size=slide_batch, shuffle=shuffle, num_workers=num_workers, drop_last=False, collate_fn=collate)
 
             return patient_subsets
